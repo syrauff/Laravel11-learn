a8e6b65ee06e353da9eac480be9c39e30c972963
syrauff <syahrul1_s1sisfo@mahasiswa.ung.ac.id>
2024-07-26 00:15:13 +0800

    First save

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Syahrul']);
+});
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
+
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
